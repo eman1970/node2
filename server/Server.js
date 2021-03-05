1181ed50fc0c8913d049ab44fbe8376abb361eb3
eman1970 <tommy.hallqvist@gmail.com>
2021-03-05 19:03:13 +0100

    Mongo DB

--- server/Server.js
+++ server/Server.js
@@ -3,53 +3,29 @@ import express, { response } from 'express'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import bodyParser from 'body-parser'
+import middlewares from './src/middlewares/Middlewares.js'
+import Configurations from './src/configurations/Configurations.js'
+import UserRoutes from './src/routes/User.route.js'
 
 dotenv.config()
-const app = express()
+const app = express() 
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
 app.use(helmet())
 app.use(morgan('common'))
-/* app.use(isAuthenticated) */
-
-
-
-const port = process.env.PORT
 
 app.get('/recipe', (request, response) => {
     response.send('Hello World!')
 })
 
-app.get('/user', isAuthenticated, (request, response) => {
+UserRoutes.routes(app)
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler)
 
-})
-
-app.use(notFound)
-app.use(errorHandler)
+Configurations.connectToDatabase()
+Configurations.connectToPort(app)
 
 
-function isAuthenticated(request, response, next) {
-    request.query.admin === 'true'
-        ? response.send('You are admin!')
-        : response.send('You cannot make calls to this API URL!')
-    next()
-}
 
-function notFound(request, response, next) {
-    const error = new Error(`Not found: ${request.originalUrl}`)
-    response.status(404)
-    next(error)
-}
-
-function errorHandler(error, request, response, next) {
-    const statuscode = response.statusCode === 200 ? 500 : response.statusCode
-    response.status(statuscode)
-    response.json({
-        statuscode: statuscode,
-        message: error.message,
-        stacktrace: error.stack,
-    })
-}
-
-app.listen(3002, () => {
-    console.log(`Servern är igång på port ${3002}`)
-
-})
+ 
---
+++ server/src/controllers/User.controller.js
@@ -0,0 +1,22 @@
+import UserModel from '../models/User.model.js'
+
+const createUser = async (request, response) => {
+
+    const user = new UserModel({
+        username: request.body.username,
+        password: request.body.password
+    })
+
+    try {
+        const databaseResponse = await user.save()
+        response.status(201).send(databaseResponse)
+    } catch(error) {
+        response.status(500).send({ message: 'Error while trying to create user',
+        stack: error })
+    }
+
+}
+
+export default {
+    createUser
+}
