db00a50bd9eca7cbf042e292245669c742f85f1b
eman1970 <tommy.hallqvist@gmail.com>
2021-03-06 12:38:23 +0100

    Formatting

--- server/Server.js
+++ server/Server.js
@@ -2,14 +2,14 @@
 import express, { response } from 'express'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
-import morgan from 'morgan' 
+import morgan from 'morgan'
 import middlewares from './src/middlewares/Middlewares.js'
 import Configurations from './src/configurations/Configurations.js'
 import UserRoutes from './src/routes/User.route.js'
 
 dotenv.config()
-const app = express() 
-app.use(express.urlencoded({ extended: true}))
+const app = express()
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(helmet())
 app.use(morgan('common'))
@@ -18,11 +18,8 @@ app.get('/recipe', (request, response) => {
     response.send('Hello World!')
 })
 
- 
-
 app.get('/auth', isAuthenticated, (request, response) => {
-
-}) 
+})
 
 UserRoutes.routes(app)
 app.use(middlewares.notFound)
@@ -36,10 +33,8 @@ function isAuthenticated(request, response, next) {
     next()
 }
 
-
 Configurations.connectToDatabase()
 Configurations.connectToPort(app)
 
 
 
- 
