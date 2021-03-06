2373ea01f5ee75753d503006b86a3b0743baaec3
eman1970 <tommy.hallqvist@gmail.com>
2021-03-06 06:53:44 +0100

    Anrop mot API

--- server/Server.js
+++ server/Server.js
@@ -2,16 +2,15 @@
 import express, { response } from 'express'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
-import morgan from 'morgan'
-import bodyParser from 'body-parser'
+import morgan from 'morgan' 
 import middlewares from './src/middlewares/Middlewares.js'
 import Configurations from './src/configurations/Configurations.js'
 import UserRoutes from './src/routes/User.route.js'
 
 dotenv.config()
 const app = express() 
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 app.use(helmet())
 app.use(morgan('common'))
 
@@ -19,9 +18,22 @@ app.get('/recipe', (request, response) => {
     response.send('Hello World!')
 })
 
+app.get('/user', isAuthenticated, (request, response) => {
+
+})
+
 UserRoutes.routes(app)
 app.use(middlewares.notFound)
 app.use(middlewares.errorHandler)
+
+
+function isAuthenticated(request, response, next) {
+    request.query.admin === 'true'
+        ? response.send('You are admin!')
+        : response.send('You cannot make calls to this API URL!')
+    next()
+}
+
 
 Configurations.connectToDatabase()
 Configurations.connectToPort(app)
